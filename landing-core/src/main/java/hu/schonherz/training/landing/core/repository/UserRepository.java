e6ef7b6c17bda01dfb120ad770d51ad556ef7f98
Bódi Balázs <bmbali1992@gmail.com>
2016-08-28 23:15:36 +0200

    registration

--- landing-core/src/main/java/hu/schonherz/training/landing/core/repository/UserRepository.java
+++ landing-core/src/main/java/hu/schonherz/training/landing/core/repository/UserRepository.java
@@ -1,15 +1,20 @@
 package hu.schonherz.training.landing.core.repository;
 
+
 import hu.schonherz.training.landing.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional(propagation = Propagation.SUPPORTS)
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByName(String name);
+    public User findByName(String name);
+
+    public List<User> getAllUser();
 
 }
--- landing-service-client-api/src/main/java/hu/schonherz/training/landing/service/UserService.java
+++ landing-service-client-api/src/main/java/hu/schonherz/training/landing/service/UserService.java
@@ -1,16 +1,18 @@
-package hu.schonherz.training.landing.service.client.api;
+package hu.schonherz.training.landing.service;
 
-import hu.schonherz.training.landing.service.vo.UserVo;
+import hu.schonherz.training.landing.vo.UserVo;
 
 import java.util.List;
 
 public interface UserService {
 
-    List<UserVo> getUsers();
+    public void createUser(UserVo userVo);
 
     UserVo getUserById(Long id);
 
     UserVo getUserByName(String name);
+
+    List<UserVo> getUsers();
 
     List<UserVo> getUsers(Integer page, Integer size);
 
