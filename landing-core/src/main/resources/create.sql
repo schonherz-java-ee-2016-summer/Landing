72f7bace2bd65c1b63c3c90e5728aa27a88d39a4
Balogh István <pistiz7@gmail.com>
2016-09-11 22:05:50 +0200

    Added actual I18N, admin role to create.sql

--- landing-core/src/main/resources/create.sql
+++ landing-core/src/main/resources/create.sql
@@ -1,1 +1,2 @@
-INSERT INTO "public".role (id, name) VALUES (1, 'ROLE_USER')
+INSERT INTO "public".role (id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO "public".role (id, name) VALUES (2, 'ROLE_ADMIN');
