6a403b0d58bc9e4b762de027d9de2c3174785aa3
Bódi Balázs <bmbali1992@gmail.com>
2016-09-12 18:45:14 +0200

    Logout and userSession added

--- landing-core/src/main/resources/create.sql
+++ landing-core/src/main/resources/create.sql
@@ -1,6 +1,6 @@
 INSERT INTO "public".roles (id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO "public".roles (id, name) VALUES (2, 'ROLE_ADMIN');
 
-INSERT INTO "public".users (id, active, email, name, password) VALUES (10000, true, 'admin@admin.hu', 'admin', '$2a$10$7z8ZcKhYICuyb5rkKhqo7uUh4je3HQocio0f5onZdugR0QTrxHDLi');
-INSERT INTO "public".users_role (user_id, roles_id) INSERT (10000, 1);
-INSERT INTO "public".users_role (user_id, roles_id) INSERT (10000, 2);
+-- INSERT INTO "public".users (id, active, email, name, password) VALUES (10000, true, 'admin@admin.hu', 'admin', '$2a$10$7z8ZcKhYICuyb5rkKhqo7uUh4je3HQocio0f5onZdugR0QTrxHDLi');
+-- INSERT INTO "public".users_role (user_id, roles_id) INSERT (10000, 1);
+-- INSERT INTO "public".users_role (user_id, roles_id) INSERT (10000, 2);
---
+++ landing-web/src/main/java/hu/schonherz/training/landing/web/managedbeans/session/UserMB.java
@@ -0,0 +1,28 @@
+package hu.schonherz.training.landing.web.managedbeans.session;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "userSession")
+@SessionScoped
+public class UserMB implements Serializable {
+
+    /*
+     *org.springframework.security.core.userdetails.User
+     */
+    private User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
